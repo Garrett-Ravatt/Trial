874bf3e67dacf242fba848bab69e5a429d28746a
Garrett-Ravatt <gravatt@calpoly.edu>
2023-10-26 18:20:19 -0700

    Message console draws itself

--- Trial/Consoles/MessageConsole.cs
+++ Trial/Consoles/MessageConsole.cs
@@ -5,39 +5,65 @@ namespace Trial.Consoles
 {
     public class MessageConsole : Console
     {
-        private InstructionSet _instructionSet;
+        private DrawString _drawString;
+        private Queue<string> _drawQueue = new Queue<string>();
+
         //private int line;
         public MessageConsole() : base(GameSettings.MessageLogWidth, GameSettings.MessageLogHeight)
         {
             // Offset to right of the map
             this.Position = new Point(GameSettings.MapWidth, 0);
 
-            // Track the instructions
-            _instructionSet = new InstructionSet();
-
             // Printing the first message
             var startMessage = ":: :: Your task comes to hand.";
-            //line = 0;
             
             Cursor.IsEnabled = false;
             Cursor.IsVisible = true;
 
             Type(startMessage);
-            Type("heh");
-            SadComponents.Add(_instructionSet);
+            Type(":: :: heh");
+            for (int i = 0; i < 10; i++)
+            {
+                Type(":: :: Let's try a line that will take up two lines of the console. How about that. Is that good?");
+            }
+            Type(":: :: epic");
         }
 
         public void Type(string message)
         {
-            // TODO: Draw animation using instructions, maybe an InstructionSet, maybe a Reader, idk.
+            if (_drawString==null || _drawString.IsFinished)
+            {
+                StartDraw(message);
+            }
+            else
+            {
+                _drawQueue.Enqueue(message);
+            }
 
-            //var typingInstruction = new DrawString(ColoredString.Parser.Parse(message));
-            //typingInstruction.Cursor = Cursor;
-            //typingInstruction.Position = Cursor.Position;
-            //typingInstruction.TotalTimeToPrint = 3f;
-            //_instructionSet.Instruct(typingInstruction);
+            //Cursor.Print(message).NewLine();
+        }
 
-            Cursor.Print(message).NewLine();
+        private void StartDraw(string message)
+        {
+            // TODO: Type in black, and have a fake Cursor run over the text, turning it white.
+
+            _drawString = new DrawString(ColoredString.Parser.Parse(message+"\n\r"));
+            _drawString.Cursor = Cursor;
+            _drawString.Position = Cursor.Position;
+            _drawString.TotalTimeToPrint = 0.7f;
+            _drawString.RemoveOnFinished = true;
+
+            _drawString.Finished += TryDequeue;
+            SadComponents.Add(_drawString);
+        }
+
+        private void TryDequeue(object? sender, EventArgs e)
+        {
+            if (_drawQueue.Count > 0)
+            {
+                var message = _drawQueue.Dequeue();
+                StartDraw(message);
+            }
         }
     }
 }
