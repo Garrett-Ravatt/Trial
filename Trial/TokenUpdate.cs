fa5d20a2efb7e8af24f4ad7e364764fc574224aa
Garrett-Ravatt <gravatt@calpoly.edu>
2023-10-27 18:20:15 -0700

    Token Draw uses fov

--- Trial/TokenUpdate.cs
+++ Trial/TokenUpdate.cs
@@ -1,4 +1,5 @@
 ﻿using Arch.Core;
+using Catalyster;
 using Catalyster.Components;
 using System.Runtime.CompilerServices;
 using Trial;
@@ -8,7 +9,10 @@ public struct TokenUpdate : IForEach<Position, Token>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Update(ref Position position, ref Token token)
     {
-        Program.MapConsole.SetGlyph(position.X, position.Y, token.Char);
-        Program.MapConsole.SetForeground(position.X, position.Y, new Color(token.Color));
+        if (GameMaster.DungeonMap.IsInFov(position.X, position.Y))
+        {
+            Program.MapConsole.SetGlyph(position.X, position.Y, token.Char);
+            Program.MapConsole.SetForeground(position.X, position.Y, new Color(token.Color));
+        }
     }
 }
