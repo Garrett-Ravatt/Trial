5f64734ba2ad01e6c1e4b5c472ee939c567410bd
Garrett-Ravatt <gravatt@calpoly.edu>
2023-10-30 13:44:19 -0700

    EventBus Muck

---
+++ CatalysterTest/Muck/EventBusMuck.cs
@@ -0,0 +1,35 @@
+﻿using Arch.Bus;
+
+// A Muck is what I'm calling tests that don't involve code from the real application.
+// Mucks are a way to see if an idea will work, then they'll be made private and removed from unit tests.
+
+namespace CatalysterTest.Muck
+{
+    public struct Message
+    {
+        public string Text;
+    }
+
+    public partial class MyInstanceReceiver
+    {
+
+        public MyInstanceReceiver() { Hook(); } // To start listening.
+
+        [Event(order: 1)]
+        public void OnStringEvent(ref Message @event)
+        {
+            Console.WriteLine(@event.Text);
+        }
+    }
+
+    [TestClass]
+    internal class EventBusMuck
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var fun = new Message { Text="funny" };
+            EventBus.Send(ref fun);
+        }
+    }
+}
