0c67b422283f0875ba27feba1829c481037f6bb3
Garrett-Ravatt <gravatt@calpoly.edu>
2023-11-02 20:05:00 -0700

    ranged attacks exist kinda

--- Catalyster/Helpers/ActionHelper.cs
+++ Catalyster/Helpers/ActionHelper.cs
@@ -1,8 +1,6 @@
 ﻿using Arch.Core;
 using Arch.Core.Extensions;
 using Catalyster.Components;
-using Catalyster.Core;
-using Catalyster.Interfaces;
 
 namespace Catalyster.Helpers
 {
@@ -49,6 +47,41 @@ namespace Catalyster.Helpers
                 return true;
             }
             
+            else
+            {
+                GameMaster.MessageLog.IDAdd(attacker, $"misses [{toHit}]");
+            }
+
+            // TODO: replace with message log
+            Console.WriteLine($"{defender} successfully defends.");
+            return false;
+        }
+
+        public static bool ResolveRanged(RangedAttack attack, Entity defender, string attacker = "")
+        {
+            //NOTE: implementation assumes defender has Defense and Health components
+            var toHit = attack.AttackFormula.Roll().Value;
+            var ac = defender.Get<Defense>().Class;
+
+            if (toHit > ac)
+            {
+                Console.WriteLine($"{defender} is hurt.");
+                ref var health = ref defender.Get<Health>();
+
+                var damage = attack.DamageFormula.Roll().Value;
+                GameMaster.MessageLog.IDAdd(attacker, $"hits [{toHit}] for {damage} damage");
+
+                health.Points -= damage;
+                if (health.Points <= 0)
+                {
+                    GameMaster.MessageLog.IDAdd(defender, "dies!");
+                    Console.WriteLine($"{defender} dies!");
+                    //TODO: needs test coverage.
+                    GameMaster.World.Destroy(defender);
+                }
+                return true;
+            }
+
             else
             {
                 GameMaster.MessageLog.IDAdd(attacker, $"misses [{toHit}]");
