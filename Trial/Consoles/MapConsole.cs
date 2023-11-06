ad44ed3635f956347558a616e4bb8b50f00c5b77
Garrett-Ravatt <gravatt@calpoly.edu>
2023-11-05 17:21:01 -0800

    Some input mapping refactorage

--- Trial/Consoles/MapConsole.cs
+++ Trial/Consoles/MapConsole.cs
@@ -1,70 +1,33 @@
-﻿using Arch.System;
-using Catalyster.Components;
+﻿using SadConsole.Components;
 using SadConsole.Input;
-using System.Runtime.CompilerServices;
+using Trial.InputStates;
 
 namespace Trial.Consoles
 {
     public class MapConsole : Console
     {
         public MapConsole(int width, int height) : base(width, height) { }
+
+        public override bool ProcessMouse(MouseScreenObjectState state)
+        {
+            Cursor.Position = state.CellPosition;
+            return base.ProcessMouse(state);
+        }
+
         public override bool ProcessKeyboard(Keyboard keyboard)
         {
             bool handled = false;
 
-            // North
-            if (keyboard.IsKeyPressed(Keys.Up) || keyboard.IsKeyPressed(Keys.NumPad8))
+            if (keyboard.IsKeyPressed(Keys.T))
             {
-                Program.GameMaster.Command.Move(0, -1);
-                handled = true;
+                // enable cursor (if item index is known)
+                Cursor.IsVisible = true;
+                Cursor.IsEnabled = true;
+                
+                // TODO: Open popup
             }
-            // South
-            else if (keyboard.IsKeyPressed(Keys.Down) || keyboard.IsKeyPressed(Keys.NumPad2))
-            {
-                Program.GameMaster.Command.Move(0, 1);
-                handled = true;
-            }
-            // West
-            else if (keyboard.IsKeyPressed(Keys.Left) || keyboard.IsKeyPressed(Keys.NumPad4))
-            {
-                Program.GameMaster.Command.Move(-1, 0);
-                handled = true;
-            }
-            // East
-            else if (keyboard.IsKeyPressed(Keys.Right) || keyboard.IsKeyPressed(Keys.NumPad6))
-            {
-                Program.GameMaster.Command.Move(1, 0);
-                handled = true;
-            }
-            // NorthEast
-            else if (keyboard.IsKeyPressed(Keys.NumPad9))
-            {
-                Program.GameMaster.Command.Move(1, -1);
-                handled = true;
-            }
-            // SouthEast
-            else if (keyboard.IsKeyPressed(Keys.NumPad3))
-            {
-                Program.GameMaster.Command.Move(1, 1);
-                handled = true;
-            }
-            // SouthWest
-            else if (keyboard.IsKeyPressed(Keys.NumPad1))
-            {
-                Program.GameMaster.Command.Move(-1, 1);
-                handled = true;
-            }
-            // NorthWest
-            else if (keyboard.IsKeyPressed(Keys.NumPad7))
-            {
-                Program.GameMaster.Command.Move(-1, -1);
-                handled = true;
-            }
-            else if (keyboard.IsKeyPressed(Keys.NumPad5))
-            {
-                Program.GameMaster.Command.Wait();
-                handled = true;
-            }
+
+            handled = MapFocus.HandleMove(handled, keyboard);
 
             if (handled)
             {
---
+++ Trial/InputStates/MapFocus.cs
@@ -0,0 +1,70 @@
+﻿using SadConsole.Input;
+
+namespace Trial.InputStates
+{
+    public static class MapFocus
+    {
+        // This is just a long chunk of the key map stuffed into a method.
+        public static bool HandleMove(bool handled, Keyboard keyboard)
+        {
+            if (handled)
+                return handled;
+
+            // North
+            else if (keyboard.IsKeyPressed(Keys.Up) || keyboard.IsKeyPressed(Keys.NumPad8))
+            {
+                Program.GameMaster.Command.Move(0, -1);
+                handled = true;
+            }
+            // South
+            else if (keyboard.IsKeyPressed(Keys.Down) || keyboard.IsKeyPressed(Keys.NumPad2))
+            {
+                Program.GameMaster.Command.Move(0, 1);
+                handled = true;
+            }
+            // West
+            else if (keyboard.IsKeyPressed(Keys.Left) || keyboard.IsKeyPressed(Keys.NumPad4))
+            {
+                Program.GameMaster.Command.Move(-1, 0);
+                handled = true;
+            }
+            // East
+            else if (keyboard.IsKeyPressed(Keys.Right) || keyboard.IsKeyPressed(Keys.NumPad6))
+            {
+                Program.GameMaster.Command.Move(1, 0);
+                handled = true;
+            }
+            // NorthEast
+            else if (keyboard.IsKeyPressed(Keys.NumPad9))
+            {
+                Program.GameMaster.Command.Move(1, -1);
+                handled = true;
+            }
+            // SouthEast
+            else if (keyboard.IsKeyPressed(Keys.NumPad3))
+            {
+                Program.GameMaster.Command.Move(1, 1);
+                handled = true;
+            }
+            // SouthWest
+            else if (keyboard.IsKeyPressed(Keys.NumPad1))
+            {
+                Program.GameMaster.Command.Move(-1, 1);
+                handled = true;
+            }
+            // NorthWest
+            else if (keyboard.IsKeyPressed(Keys.NumPad7))
+            {
+                Program.GameMaster.Command.Move(-1, -1);
+                handled = true;
+            }
+            else if (keyboard.IsKeyPressed(Keys.NumPad5))
+            {
+                Program.GameMaster.Command.Wait();
+                handled = true;
+            }
+
+            return handled;
+        }
+    }
+}
