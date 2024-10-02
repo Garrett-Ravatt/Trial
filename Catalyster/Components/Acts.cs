400bb7d25285dc00ca34b8d664f2c9bce72fb663
Garrett-Ravatt <gravatt@calpoly.edu>
2024-10-01 17:41:17 -0700

    The first Act is now added

--- Catalyster/Components/Acts.cs
+++ Catalyster/Components/Acts.cs
@@ -1,4 +1,6 @@
 ﻿using Arch.Core;
+using Arch.Core.Extensions;
+using Catalyster.Helpers;
 using Catalyster.Interfaces;
 using System.Transactions;
 
@@ -19,6 +21,33 @@ namespace Catalyster.Components
 
         public bool Enter(Entity entity, World world)
         {
+            ref var energy = ref entity.Get<Energy>();
+
+            ref var position = ref entity.Get<Position>();
+            var newPos = new Position { X = position.X + X, Y = position.Y + Y };
+
+            if (GameMaster.DungeonMap.IsWalkable(newPos.X, newPos.Y))
+            {
+                Entity? bumped = null;
+                if (SpatialHelper.ClearOrAssign(position.X + X, position.Y + Y, ref bumped))
+                {
+                    position = newPos;
+                    // TODO: refer to movement speed
+                    energy.Points -= WiggleHelper.Wiggle(1000, .1);
+                }
+
+                else // ran into a creature
+                {
+                    ActionHelper.ResolveAttack(entity, bumped.Value);
+                    // TODO: refer to attack cost
+                    energy.Points -= WiggleHelper.Wiggle(1000, .1);
+                }
+            }
+
+            else if (entity.Has<Player>()) {
+                GameMaster.MessageLog.Add("You bump into the wall. You fool.");
+            }
+
             return true;
         }
     }
--- CatalysterTest/ActTesting/ActTest.cs
+++ CatalysterTest/ActTesting/ActTest.cs
@@ -1,14 +1,35 @@
-﻿using Catalyster.Components;
+﻿using CatalysterTest.TestUtils;
+using Catalyster.Components;
+using Catalyster;
+using Arch.Core.Extensions;
 
 namespace CatalysterTest.ActTesting
 {
     [TestClass]
     public class ActTest
     {
+
         [TestMethod]
         public void ActTest1()
         {
-            var act = new MoveAct();
+            var act1 = new MoveAct(0, 1);
+            var act2 = new MoveAct();
+        }
+
+        [TestMethod]
+        public void ActTest2()
+        {
+            var gm = new GameMaster();
+            GameMaster.DungeonMap.Initialize(40, 40);
+            GameMaster.DungeonMap.SetAllWalkable();
+
+            var player = ExFactory.SimpleCreature(GameMaster.World);
+            var act = new MoveAct(0, 1);
+
+            var Y = player.Get<Position>().Y;
+
+            Assert.IsTrue(act.Enter(player, GameMaster.World));
+            Assert.AreNotEqual(Y, player.Get<Position>().Y);
         }
     }
 }
