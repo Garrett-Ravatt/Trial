d0b9faaed805effd4694923bfae27f98d7b0b754
Garrett-Ravatt <gravatt@calpoly.edu>
2024-11-16 16:27:18 -0800

    zero cost action bc why noooot

--- Catalyster/Acts/CommandInjectionAct.cs
+++ Catalyster/Acts/CommandInjectionAct.cs
@@ -4,7 +4,7 @@ namespace Catalyster.Acts
 {
     public struct CommandInjectionAct : IAct
     {
-        public int Cost { get; set; } = 1000;
+        public int Cost { get; set; } = 0;
         public bool Resolved { get; set; } = false;
         public bool Suspended { get { if (Resolved) return false; return InjectedAct == null; } }
         public static IAct? InjectedAct { get; set; } = null;
